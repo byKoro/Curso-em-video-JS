fd47479f0d8eff5bf7e96e071fedc138e0875299
BuildTools <unconfigured@null.spigotmc.org>
2023-10-02 21:32:04 -0300

    feat: curso-em-video-js

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 60
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+if (velocidade > 60) { //Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log('Use cinto de segurança')
