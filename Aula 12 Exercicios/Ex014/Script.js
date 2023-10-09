347cc48e1fdb1de0f5eddef40125c37cda0a8922
BuildTools <unconfigured@null.spigotmc.org>
2023-10-09 20:24:50 -0300

    Atualização dos Arquivos

--- Aula 12 Exercicios/Ex014/Script.js
+++ Aula 12 Exercicios/Ex014/Script.js
@@ -1,26 +1,42 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var horas = data.getHours()
-    msg.innerHTML = `Agora são ${horas} horas`
-    var style = document.body.style
-    if (horas >= 0 && horas <= 12) {
-        // BOM DIA
-        msg.innerHTML = `Olá Mundo, Agora são ${horas} horas, tenha um Bom Dia !`
-        img.src = "Manhã.png"
-        style.background = '#fbca96'
-        
-    }else if (horas >= 13 && horas < 18 ) {
-        //BOA TARDE
-        msg.innerHTML = `Olá Mundo, Agora são ${horas} horas, tenha uma Boa Tarde !`
-        img.src = "Tarde.png"
-        style.background = '#ef6a00'
-    }else {
-        //BOA NOITE
-        msg.innerHTML = `Olá Mundo, Agora são ${horas} horas, tenha uma Boa Noite !`
-        img.src = "Noite.png"
-        style.background = '#001f35'
-        
+var ano_atual = new Date().getFullYear() // Pegar o ano atual;
+
+function identificar() {
+    let input_ano = document.getElementById("txtano").value
+    
+    function calcularIdade(){
+        let Idade = ano_atual - input_ano
+        return Idade
     }
+    calcularIdade() // Calcula o ano digitado menos o ano atual;
+
+    function sexoDefinir() {
+        let Sexo = document.getElementsByName("radsex")
+        Sexo[0].checked ? Sexo = 'Masculino' : Sexo = 'Feminino'
+        return Sexo
+    }
+    sexoDefinir()   // Salva o sexo selecionado pelo usuário;
+
+    function mostrarResultado(Sexo , Idade) {
+        var mostrarResultado = document.getElementById('res')
+        mostrarResultado.innerHTML = `Você é uma pessoa do sexo : ${Sexo} <br>Sua idade é : ${Idade} anos`
+        mostrarResultado;
+    }
+    mostrarResultado(sexoDefinir(),calcularIdade()) // Mostrar o resultado na tela;
+
+    function mostrarImagem(Sexo , Idade) {
+        function imagem(escolherFoto) {
+            let localImagem = document.getElementById('foto')
+            localImagem.setAttribute("src" , escolherFoto)
+        }        
+        if (Idade >= 0 && Idade < 10) { // Criança
+            Sexo    ==  'Masculino' ?   imagem("homem-criança.png")   :   imagem("mulher-criança.png")
+        }else if (Idade < 21) { // Adolescente
+            Sexo    ==  'Masculino' ?   imagem("homem-adolescente.png")   :   imagem("mulher-adolescente.png")
+        }else if(Idade < 50) {  // Adulto
+            Sexo    ==  'Masculino' ?   imagem("homem-adulto.png")  :   imagem("mulher-adulto.png")
+        }else { // Idoso
+            Sexo    ==  'Masculino' ?   imagem("homem-idoso.png")   :   imagem("mulher-idosa.png")
+        }
+    }
+    mostrarImagem(sexoDefinir() , calcularIdade())
 }
